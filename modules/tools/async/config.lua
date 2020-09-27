6a23cf77b39f45f5ec69f858186251358c9e3404
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2020-09-27 16:18:10 -0400

    git and async lazy loading fixed

--- modules/tools/async/config.lua
+++ modules/tools/async/config.lua
@@ -1,2 +1,2 @@
-vim.cmd'command! AsyncRun packadd asyncrun.vim | AsyncRun <args>'
-vim.cmd'command! -nargs=* Make AsyncRun -strip -post=copen -program=make <args>'
+vim.cmd'command! -bang -nargs=* AsyncRun packadd asyncrun.vim | AsyncRun<bang> <args>'
+vim.cmd"command! -bang -nargs=* Make AsyncRun<bang> -strip -post=copen -program=make <args>"
--- modules/tools/git/config.lua
+++ modules/tools/git/config.lua
@@ -1,2 +1,2 @@
-vim.cmd'command! G packadd vim-fugitive | edit | G <args>'
+vim.cmd'command! G packadd vim-fugitive | silent! edit | G <args>'
 vim.cmd'nnoremap <silent> <Leader>g <cmd>belowright G<CR>'
