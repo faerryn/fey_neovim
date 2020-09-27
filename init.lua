9817525939d40aed9444c7c191652ff143a2bee1
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2020-09-27 15:53:17 -0400

    reloading config files

--- init.lua
+++ init.lua
@@ -16,29 +16,39 @@ if vim.g.batch ~= nil then
 	minpac.add('k-takata/minpac', {type = 'opt'})
 end
 
-local function load_module(module_d, module_name)
-	if vim.g.batch ~= nil then
+vim.cmd'augroup fey_reload'
+vim.cmd'augroup END'
+
+if vim.g.batch ~= nil then
+	function fey_load_module(module_d, module_name)
 		local packages_f = module_d .. '/packages.lua'
 		if vim.fn.filereadable(packages_f) ~= 0 then dofile(packages_f) end
-	else
+	end
+else
+	function fey_load_module(module_d, module_name)
+		local config_f = module_d .. '/config.lua'
+		vim.cmd('autocmd! fey_reload BufWritePost ' .. vim.fn.resolve(config_f) .. ' lua fey_load_module(' .. module_d .. ', ' .. module_name .. ')')
+
 		local augroup = 'fey_' .. module_name:gsub('/', '_')
 		vim.cmd('augroup ' .. augroup)
 		vim.cmd('autocmd! ' .. augroup)
 		vim.cmd('augroup END')
-		local config_f = module_d .. '/config.lua'
+
 		if vim.fn.filereadable(config_f) ~= 0 then dofile(config_f) end
 	end
 end
+
+local fey_core_d = vim.fn.expand'<sfile>:p:h'
 
 local config_d = vim.fn.expand'$XDG_CONFIG_HOME'
 if config_d == '' then config_d = vim.fn.expand'$HOME/.config' end
 local fey_d = config_d .. '/fey'
 
-load_module(vim.fn.stdpath'config', 'core')
-load_module(fey_d, 'user')
+fey_load_module(fey_core_d, 'core')
+fey_load_module(fey_d, 'user')
 
 local init_f = fey_d .. '/init.lua'
-local modules_d = vim.fn.stdpath'config' .. '/modules'
+local modules_d = fey_core_d .. '/modules'
 local user_modules_d = fey_d .. '/modules'
 
 for category, modules in pairs(dofile(init_f)) do
@@ -46,9 +56,9 @@ for category, modules in pairs(dofile(init_f)) do
 		local category_module_d = category .. '/' .. module
 		local user_category_module_d = user_modules_d .. '/' .. category_module_d
 		if vim.fn.isdirectory(user_category_module_d) ~= 0 then
-			load_module(user_category_module_d, category_module_d)
+			fey_load_module(user_category_module_d, category_module_d)
 		else
-			load_module(modules_d .. '/' .. category_module_d, category_module_d)
+			fey_load_module(modules_d .. '/' .. category_module_d, category_module_d)
 		end
 	end
 end
