20e65de5cfad461172b9b5e00479fa9eb2ec1eb5
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2020-09-27 15:21:57 -0400

    single quotes vs double quotes

--- modules/tools/async/config.lua
+++ modules/tools/async/config.lua
@@ -1,2 +1,2 @@
 vim.cmd'command! AsyncRun packadd asyncrun.vim | AsyncRun <args>'
-vim.cmd"command! -nargs=* Make AsyncRun -strip -post=copen -program=make <args>"
+vim.cmd'command! -nargs=* Make AsyncRun -strip -post=copen -program=make <args>'
