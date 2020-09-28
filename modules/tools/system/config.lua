751411f0ea91977e669a9f5f4cf01d0739499602
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2020-09-28 11:10:05 -0400

    fixed the eunuch lazy load

--- modules/tools/system/config.lua
+++ modules/tools/system/config.lua
@@ -1,15 +1,16 @@
-vim.cmd'command! -bar -bang Unlink packadd vim-eunuch <Bar> Unlink<bang>'
+local setup_vim_eunuch = 'delcommand SudoWrite <Bar> packadd vim-eunuch'
+vim.cmd('command! -bar -bang Unlink ' .. setup_vim_eunuch .. ' <Bar> Unlink<bang>')
 vim.cmd'command! -bar -bang Remove Unlink<bang>'
-vim.cmd'command! -bar -bang Delete packadd vim-eunuch <Bar> Delete<bang>'
-vim.cmd'command! -bar -nargs=1 -bang -complete=file Move packadd vim-eunuch <Bar> Move<bang> <args>'
-vim.cmd'command! -bar -nargs=1 -bang -complete=file Rename packadd vim-eunuch <Bar> Rename<bang> <args>'
-vim.cmd'command! -bar -bang -nargs=+ Chmod packadd vim-eunuch <Bar> Chmod<bang> <args>'
-vim.cmd'command! -bar -bang -nargs=? -complete=dir Mkdir packadd vim-eunuch <Bar> Mkdir<bang> <args>'
-vim.cmd'command! -bar -bang -complete=file -nargs=+ Cfind   packadd vim-eunuch <Bar> Cfind<bang> <args>'
-vim.cmd'command! -bar -bang -complete=file -nargs=+ Clocate packadd vim-eunuch <Bar> Clocate<bang> <args>'
-vim.cmd'command! -bar -bang -complete=file -nargs=+ Lfind   packadd vim-eunuch <Bar> Lfind<bang> <args>'
-vim.cmd'command! -bar -bang -complete=file -nargs=+ Llocate packadd vim-eunuch <Bar> Llocate<bang> <args>'
-vim.cmd'command! -bar -bang -complete=file -nargs=? SudoEdit packadd vim-eunuch <Bar> SudoEdit<bang> <args>'
-vim.cmd'command! -bar SudoWrite delcommand SudoWrite <Bar> packadd vim-eunuch <Bar> SudoEdit'
-vim.cmd'command! -bar -nargs=? Wall packadd vim-eunuch <Bar> Wall <args>'
+vim.cmd('command! -bar -bang Delete ' .. setup_vim_eunuch .. ' <Bar> Delete<bang>')
+vim.cmd('command! -bar -nargs=1 -bang -complete=file Move ' .. setup_vim_eunuch .. ' <Bar> Move<bang> <args>')
+vim.cmd('command! -bar -nargs=1 -bang -complete=file Rename ' .. setup_vim_eunuch .. ' <Bar> Rename<bang> <args>')
+vim.cmd('command! -bar -bang -nargs=+ Chmod ' .. setup_vim_eunuch .. ' <Bar> Chmod<bang> <args>')
+vim.cmd('command! -bar -bang -nargs=? -complete=dir Mkdir ' .. setup_vim_eunuch .. ' <Bar> Mkdir<bang> <args>')
+vim.cmd('command! -bar -bang -complete=file -nargs=+ Cfind   ' .. setup_vim_eunuch .. ' <Bar> Cfind<bang> <args>')
+vim.cmd('command! -bar -bang -complete=file -nargs=+ Clocate ' .. setup_vim_eunuch .. ' <Bar> Clocate<bang> <args>')
+vim.cmd('command! -bar -bang -complete=file -nargs=+ Lfind   ' .. setup_vim_eunuch .. ' <Bar> Lfind<bang> <args>')
+vim.cmd('command! -bar -bang -complete=file -nargs=+ Llocate ' .. setup_vim_eunuch .. ' <Bar> Llocate<bang> <args>')
+vim.cmd('command! -bar -bang -complete=file -nargs=? SudoEdit ' .. setup_vim_eunuch .. ' <Bar> SudoEdit<bang> <args>')
+vim.cmd('command! -bar SudoWrite ' .. setup_vim_eunuch .. ' <Bar> SudoEdit')
+vim.cmd('command! -bar -nargs=? Wall ' .. setup_vim_eunuch .. ' <Bar> Wall <args>')
 vim.cmd'command! -bar W Wall'
