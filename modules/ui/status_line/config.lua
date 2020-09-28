edd303c9de9c2d3caf955c3ffa0667d967cab228
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2020-09-28 01:57:06 -0400

    more elegant statusline pre-VimEnter detection

--- modules/ui/status_line/config.lua
+++ modules/ui/status_line/config.lua
@@ -1,5 +1,7 @@
 function fey_ui_status_line_lightline()
 	vim.g.lightline = { tabline = { right = {} }, colorscheme = vim.g.colors_name or 'default' }
-	pcall(vim.fn['lightline#enable'])
+	if vim.g.loaded_lightline ~= nil then
+		vim.fn['lightline#enable']()
+	end
 end
 vim.cmd'autocmd fey_ui_status_line VimEnter,ColorScheme * lua fey_ui_status_line_lightline()'
