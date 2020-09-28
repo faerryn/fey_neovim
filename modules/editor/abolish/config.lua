a487fbb6cb3bc83ceb60d4d27918a3b95072b3b9
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2020-09-28 01:49:07 -0400

    no custom complete before packadd

--- modules/editor/abolish/config.lua
+++ modules/editor/abolish/config.lua
@@ -1,4 +1,4 @@
 vim.cmd'nmap cr <Cmd>packadd vim-abolish<CR>cr'
 vim.cmd'command! -nargs=+ -bang -bar -range=0 Abolish packadd vim-abolish <Bar> <line1>,<line2>Abolish<bang> <args>'
 vim.cmd'command! -nargs=1 -bang -bar -range=0 Subvert packadd vim-abolish <Bar> <line1>,<line2>Subvert<bang> <args>'
-vim.cmd'command -nargs=1 -bang -bar -range=0 -complete=custom,s:SubComplete S <line1>,<line2>Subvert<bang> <args>'
+vim.cmd'command -nargs=1 -bang -bar -range=0 S <line1>,<line2>Subvert<bang> <args>'
