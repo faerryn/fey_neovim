3108ea505875cb44d9e925dedecec79a1408482f
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2020-09-28 10:40:02 -0400

    Comments and documentation to config.lua and core.lua

--- config.lua
+++ config.lua
@@ -6,12 +6,38 @@ vim.wo.number         = true
 vim.wo.relativenumber = true
 vim.wo.signcolumn     = 'number'
 
--- Persistent Files
-vim.o.shadafile       = vim.fn.stdpath'cache' .. '/shada/main.shada'
-vim.o.directory       = vim.fn.stdpath'cache' .. '/swap'
-vim.o.undodir         = vim.fn.stdpath'cache' .. '/undo'
-vim.o.undofile        = true
-vim.bo.undofile       = true
+-- Spelling
+vim.o.spell           = true
+vim.wo.spell          = true
+vim.o.spellcapcheck   = ''
+vim.bo.spellcapcheck  = ''
+
+-- UI
+vim.o.laststatus      = 2
+vim.o.showcmd         = false
+vim.o.showmode        = false
+vim.o.showtabline     = 2
+
+-- Splits
+vim.o.equalalways     = false
+vim.o.splitbelow      = true
+vim.o.splitright      = true
+
+-- Wrapping
+vim.wo.linebreak      = true
+vim.wo.wrap           = true
+
+-- Misc setting
+vim.o.confirm         = true
+vim.o.foldlevelstart  = 99
+vim.o.inccommand      = 'nosplit'
+vim.o.iskeyword       = 'a-z,A-Z,48-57,_,-'
+vim.bo.iskeyword      = 'a-z,A-Z,48-57,_,-'
+vim.o.lazyredraw      = true
+vim.o.nrformats       = 'alpha,octal,hex,bin'
+vim.bo.nrformats      = 'alpha,octal,hex,bin'
+vim.o.omnifunc        = 'syntaxcomplete#Complete'
+vim.bo.omnifunc       = 'syntaxcomplete#Complete'
 
 -- ripgrep
 if vim.fn.executable'rg' then
@@ -19,10 +45,10 @@ if vim.fn.executable'rg' then
 	vim.o.grepformat = '%f:%l:%c:%m'
 end
 
--- yank highlighting
+-- Yank highlighting
 vim.cmd'autocmd TextYankPost * lua vim.highlight.on_yank{timeout=500}'
 
--- return cursor to its last position when opening a file
+-- Remember last cursor position
 vim.cmd'autocmd BufReadPost * lua if vim.bo.filetype ~= "gitcommit" then pcall(vim.api.nvim_win_set_cursor, 0, vim.api.nvim_buf_get_mark(0, "\\"")) end'
 
 -- api
--- core.lua
+++ core.lua
@@ -1,42 +1,31 @@
+-- core.lua
+-- Essential settings for lots of goodies
 vim.o.hidden          = true
 vim.o.clipboard       = 'unnamedplus'
-vim.o.confirm         = true
-vim.o.foldlevelstart  = 99
-vim.o.inccommand      = 'nosplit'
-vim.o.iskeyword       = 'a-z,A-Z,48-57,_,-'
-vim.bo.iskeyword      = 'a-z,A-Z,48-57,_,-'
-vim.o.lazyredraw      = true
 vim.o.mouse           = 'ar'
-vim.o.nrformats       = 'alpha,octal,hex,bin'
-vim.bo.nrformats      = 'alpha,octal,hex,bin'
-vim.o.omnifunc        = 'syntaxcomplete#Complete'
-vim.bo.omnifunc       = 'syntaxcomplete#Complete'
-vim.o.spell           = true
-vim.wo.spell          = true
-vim.o.spellcapcheck   = ''
-vim.bo.spellcapcheck  = ''
 
 vim.o.timeoutlen      = 500
 vim.o.updatetime      = 500
 
-vim.o.equalalways     = false
-vim.o.laststatus      = 2
-vim.wo.linebreak      = true
-vim.o.showcmd         = false
-vim.o.showmode        = false
-vim.o.showtabline     = 2
-vim.o.splitbelow      = true
-vim.o.splitright      = true
-vim.wo.wrap           = true
-
 vim.o.termguicolors   = true
-vim.cmd'doautocmd ColorScheme'
 
-vim.cmd'autocmd FileType qf nnoremap <silent> <buffer> q <C-W>c'
-vim.cmd'noremap <silent> <Leader> <Nop>'
-vim.cmd'noremap <silent> s <Nop>'
-
+-- Y for y$ instead of yy
 vim.cmd'nnoremap Y y$'
 
+-- Persistent Files
+vim.o.shadafile       = vim.fn.stdpath'cache' .. '/shada/main.shada'
+vim.o.directory       = vim.fn.stdpath'cache' .. '/swap'
+vim.o.undodir         = vim.fn.stdpath'cache' .. '/undo'
+vim.o.undofile        = true
+vim.bo.undofile       = true
+
+-- q to quit quickfix and location list
+vim.cmd'autocmd FileType qf nnoremap <silent> <buffer> q <C-W>c'
+
+-- Disable s and S (equivalent to C and cc)
+vim.cmd'noremap <silent> S <Nop>'
+vim.cmd'noremap <silent> s <Nop>'
+
+-- Disable netrw
 vim.g.loaded_netrw       = 1
 vim.g.loaded_netrwPlugin = 1
