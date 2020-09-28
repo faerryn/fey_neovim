deb9f65ead90661df774290c46980090a7afec72
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2020-09-28 10:40:19 -0400

    vim-fugitive lazy loading on :G now actually works

--- modules/tools/git/config.lua
+++ modules/tools/git/config.lua
@@ -1,2 +1,2 @@
-vim.cmd'command! -bang -nargs=? -range=-1 -complete=customlist,fugitive#Complete G packadd vim-fugitive | silent! edit | exe fugitive#Command(<line1>, <count>, +"<range>", <bang>0, "<mods>", <q-args>)'
+vim.cmd'command! -bang -nargs=? -range=-1 -complete=customlist,fugitive#Complete G packadd vim-fugitive <Bar> call FugitiveDetect(expand("%")) <Bar> exe fugitive#Command(<line1>, <count>, +"<range>", <bang>0, "<mods>", <q-args>)'
 vim.cmd'nnoremap <silent> <Leader>g <cmd>belowright G<CR>'
