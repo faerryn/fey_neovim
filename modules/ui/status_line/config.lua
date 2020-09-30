a340adcf391acfde7dc4ac63a8621f4658c474d4
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2020-09-30 17:07:39 -0400

    lightline packadd on vimrc run, not VimEnter

--- modules/ui/status_line/config.lua
+++ modules/ui/status_line/config.lua
@@ -1,8 +1,6 @@
 fey.ui_status_line.lightline = function()
-	if vim.g.loaded_lightline == nil then
-		vim.cmd'packadd lightline.vim'
-	end
 	vim.g.lightline = { tabline = { right = {} }, colorscheme = vim.g.colors_name or 'default' }
 	vim.fn['lightline#enable']()
 end
 vim.cmd'autocmd VimEnter,ColorScheme * lua fey.ui_status_line.lightline()'
+vim.cmd'packadd lightline.vim'
