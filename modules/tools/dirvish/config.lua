48a9b9941e51390bfb1285b9252d5e7bebd06f93
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2020-10-01 10:51:50 -0400

    fix dirvish, proper lazy-loading

--- modules/tools/dirvish/config.lua
+++ modules/tools/dirvish/config.lua
@@ -3,4 +3,4 @@ vim.g.loaded_netrw       = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- load up vim-dirvish
-vim.cmd'autocmd BufEnter * if isdirectory(expand("%")) | packadd vim-dirvish | set filetype=dirvish | edit | autocmd! fey_tools_dirvish'
+vim.cmd'autocmd BufEnter * lua if vim.fn.isdirectory(vim.fn.expand("%")) ~= 0 then vim.cmd"autocmd! fey_tools_dirvish"; vim.cmd"packadd vim-dirvish"; vim.bo.filetype = "dirvish"; vim.cmd"Dirvish"; end'
