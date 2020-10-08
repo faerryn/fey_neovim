c06b3e2754b67159a6319299f72624ad3ebfddc3
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2020-10-08 10:54:36 -0400

    remove fugitive lazy-load

--- modules/tools/git/config.lua
+++ modules/tools/git/config.lua
@@ -1,2 +1,2 @@
-vim.cmd'command! -bang -nargs=? -range=-1 -complete=customlist,fugitive#Complete G packadd vim-fugitive <Bar> call FugitiveDetect(expand("%")) <Bar> exe fugitive#Command(<line1>, <count>, +"<range>", <bang>0, "<mods>", <q-args>)'
+vim.cmd'packadd vim-fugitive'
 vim.cmd'nnoremap <silent> <Leader>g <cmd>belowright G<CR>'
