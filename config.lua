2ca5ed46b2b0bc4708bf5236487d30d847360b26
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2020-10-09 19:43:08 -0400

    ripgrep should use all threads

--- config.lua
+++ config.lua
@@ -66,7 +66,7 @@ vim.bo.omnifunc       = 'syntaxcomplete#Complete'
 
 -- ripgrep
 if vim.fn.executable'rg' then
-	vim.o.grepprg    = '{2>/dev/null rg --hidden --threads=1 --ignore-file $XDG_CONFIG_HOME/rg/ignore --vimgrep $*}'
+	vim.o.grepprg    = '{2>/dev/null rg --hidden --ignore-file $XDG_CONFIG_HOME/rg/ignore --vimgrep $*}'
 	vim.o.grepformat = '%f:%l:%c:%m'
 end
 
